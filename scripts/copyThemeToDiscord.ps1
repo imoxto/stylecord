883631628b3022e7e89542a413e3e7b639a5de38
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2024-01-17 23:55:55 +0600

    remove res

--- scripts/copyThemeToDiscord.ps1
+++ scripts/copyThemeToDiscord.ps1
@@ -2,9 +2,11 @@
  
 # This is the directory you want to copy to the computer (IE. c:\folder_to_be_copied)
 $source = "styles\*.css"
+$resSrc = "res\*.jpg"
  
 # On the desination computer, where do you want the folder to be copied?
 $dest = "C:\Users\$env:USERNAME\AppData\Roaming\Vencord\themes"
 
 Copy-Item -Path $source -Destination $dest -Recurse
+Copy-Item -Path $resSrc -Destination $dest -Recurse
 
