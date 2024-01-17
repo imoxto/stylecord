8b86c4776a9da5942aec5735eb12632a7d1776e8
imoxto <mohammad.rafid.hamid@g.bracu.ac.bd>
2024-01-17 22:35:07 +0600

    refactor colors

---
+++ scripts/copyThemeToDiscord.ps1
@@ -0,0 +1,10 @@
+
+ 
+# This is the directory you want to copy to the computer (IE. c:\folder_to_be_copied)
+$source = "styles\*.css"
+ 
+# On the desination computer, where do you want the folder to be copied?
+$dest = "C:\Users\$env:USERNAME\AppData\Roaming\Vencord\themes"
+
+Copy-Item -Path $source -Destination $dest -Recurse
+
